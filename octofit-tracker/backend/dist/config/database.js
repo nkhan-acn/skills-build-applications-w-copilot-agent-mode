72d364747245569e15c68f30bca6c12b115f05c9
nkhan-acn <nazneen.b.khan@accenture.com>
2026-07-01 17:59:01 +0000

    stragueling change

---
+++ octofit-tracker/backend/dist/config/database.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.mongoUri = void 0;
+exports.connectToDatabase = connectToDatabase;
+const mongoose_1 = __importDefault(require("mongoose"));
+exports.mongoUri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/octofit_db';
+async function connectToDatabase() {
+    await mongoose_1.default.connect(exports.mongoUri);
+    return mongoose_1.default.connection;
+}
